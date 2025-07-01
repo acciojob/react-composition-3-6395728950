ed7d0d6c917c53501d4e1e8069a85e7083f4349e
kuldeepparmar <kuldeep07122002@gmail.com>
2025-07-01 16:06:53 +0530

    Done

---
+++ src/components/Tooltip.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+const Tooltip = ({text,children}) => {
+    const[show,setShow] = useState(false);
+  return (
+    <div 
+    onMouseEnter={()=>setShow(true)}
+    onMouseLeave={()=>setShow(false)}
+   className='tooltip'
+    >
+    
+   {
+    show &&(
+        <div className='tooltiptext'>
+            {text}
+        </div>
+    )
+   }
+   {children}
+
+    </div>
+  )
+}
+
+export default Tooltip
